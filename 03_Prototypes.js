f8d4e26d0124e8d33c162670773a80525ad2402a
Yash <yaswanth.iddibani@gmail.com>
2020-05-28 21:57:58 +0100

    Prototypes

--- 03_Prototypes.js
+++ 03_Prototypes.js
@@ -3,14 +3,43 @@
 function Person(firstName, lastName, dob) {
   (this.firstName = firstName),
     (this.lastName = lastName),
-    (this.birthday = new Date(dob)),
-    (this.calculateAge = function () {
-      const diff = Date.now() - this.birthday.getTime();
-      const ageDate = new Date(diff);
-      return Math.abs(ageDate.getUTCFullYear() - 1970);
-    });
+    (this.birthday = new Date(dob));
+    // (this.calculateAge = function () {
+    //   const ageDiff = Date.now() - this.birthday.getTime();
+    //   const ageDate = new Date(ageDiff);
+    //   return Math.abs(ageDate.getUTCFullYear()-1970);
+    // });
 }
+
+// Calculate Age
+Person.prototype.calculateAge = function(){
+  const ageDiff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(ageDiff);
+  return Math.abs(ageDate.getUTCFullYear()-1970);
+};
+
+Person.prototype.getFullName = function(){
+  return `${this.firstName} ${this.lastName}`;
+}
+
+// Gets Married
+Person.prototype.getMarried = function(newLastName){
+  this.lastName = newLastName;
+}
+
 const yash = new Person("Yaswanth", "Iddibani", "01-11-1991");
-const sai = new Person("Sai", "Iddibani", "02-11-1998");
+const amulya = new Person("Amulya", "D", "02-11-1998");
 
 console.log(yash);
+console.log(yash.calculateAge());
+console.log(yash.getFullName());
+
+amulya.getMarried('Yaswanth');
+console.log(amulya.getFullName());
+
+// Has Own Property
+console.log(yash.hasOwnProperty('firstName'));
+console.log(yash.hasOwnProperty('getFullName'));
+
+
+
